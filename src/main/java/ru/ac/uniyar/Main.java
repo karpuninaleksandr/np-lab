889c762cbab78b7805dc105dd7cfd2dfd08c7f6f
aleksandrkarpunin <aleksandrkarpunin0889@gmail.com>
2025-04-17 16:17:40 +0300

    algorithm improvements

--- src/main/java/ru/ac/uniyar/Main.java
+++ src/main/java/ru/ac/uniyar/Main.java
@@ -13,13 +13,13 @@ public class Main {
         Writer writer = new Writer();
         BenchmarkResultValidator validator = new BenchmarkResultValidator();
         SpanningTreeFinder spanningTreeFinder = new SpanningTreeFinder();
-        int taskSize = 4096, tryNumber = 3;
+        int taskSize = 2048, tryNumber = 3;
 
         Task task = reader.readTask(String.format("src/main/resources/benchmark/Taxicab_%s.txt", taskSize));
-//        BenchmarkResult result = spanningTreeFinder.findMinSpanningTree(task);
-//        System.out.println("size: " + taskSize + ", weight: " + result.getWeight() + ", leaves: " + result.getLeaves());
-//        writer.writeResult(result, String.format("src/main/resources/result/task_1/try_%s/Karpunin_%s_%s.txt", tryNumber, taskSize, tryNumber));
-        Result result = reader.readResult(String.format("src/main/resources/result/task_1/try_3/Karpunin_%s_%s_229184.txt", taskSize, tryNumber));
+        Result result = spanningTreeFinder.findMinSpanningTree(task);
+        System.out.println("size: " + taskSize + ", weight: " + result.getWeight() + ", leaves: " + result.getLeaves());
+        writer.writeResult(result, String.format("src/main/resources/result/task_1/try_%s/Karpunin_%s_%s.txt", tryNumber, taskSize, tryNumber));
+//        Result result = reader.readResult("src/main/resources/result/task_1/try_3/9477_Karpunin_512_3.txt");
         validator.validate(task, result);
     }
 }
--- src/main/java/ru/ac/uniyar/service/SpanningTreeFinder.java
+++ src/main/java/ru/ac/uniyar/service/SpanningTreeFinder.java
@@ -4,31 +4,19 @@ import ru.ac.uniyar.model.*;
 import ru.ac.uniyar.utils.Utils;
 
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.concurrent.*;
 
 public class SpanningTreeFinder {
     public Result findMinSpanningTree(Task task) {
         int n = task.getSize();
         int maxLeaves = n / 16;
-        Map<Integer, Map<Integer, Integer>> weights = new HashMap<>();
+        int[][] weights = new int[n + 1][n + 1];
 
         for (int i = 1; i <= n; ++i) {
-            Map<Integer, Integer> currWeights = new HashMap<>();
             for (int it = 1; it <= n; ++it) {
                 if (i == it) continue;
-                currWeights.put(it, Utils.getDistance(task.getVertexes().get(i), task.getVertexes().get(it)));
+                weights[i][it] = Utils.getDistance(task.getVertexes().get(i), task.getVertexes().get(it));
             }
-            Map<Integer, Integer> sortedWeights = currWeights.entrySet()
-                    .stream()
-                    .sorted(Map.Entry.comparingByValue())
-                    .collect(Collectors.toMap(
-                            Map.Entry::getKey,
-                            Map.Entry::getValue,
-                            (oldValue, newValue) -> oldValue,
-                            LinkedHashMap::new));
-            sortedWeights.putAll(currWeights);
-            weights.put(i, sortedWeights);
         }
 
         ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
@@ -55,22 +43,18 @@ public class SpanningTreeFinder {
                 .orElse(null);
     }
 
-    private Result checkVertex(int k, int n, int maxLeaves, Map<Integer, Map<Integer, Integer>> weights) {
-        Map<Integer, Boolean> spanningTree = new HashMap<>();
-        Map<Integer, Integer> degrees = new HashMap<>();
-        for (int i = 1; i <= n; ++i) {
-            spanningTree.put(i, false);
-            degrees.put(i, 0);
-        }
+    private Result checkVertex(int k, int n, int maxLeaves, int[][] weights) {
+        boolean[] spanningTree = new boolean[n + 1];
+        int[] degrees = new int[n + 1];
 
         PriorityQueue<Edge> queue = new PriorityQueue<>(Comparator.comparingInt(Edge::getWeight));
         Result result = new Result();
         result.setEdges(new ArrayList<>());
 
-        spanningTree.put(k, true);
+        spanningTree[k] = true;
         for (int i = 1; i <= n; ++i) {
             if (i != k) {
-                queue.offer(new Edge(k, i, weights.get(k).get(i)));
+                queue.offer(new Edge(k, i, weights[k][i]));
             }
         }
 
@@ -79,110 +63,132 @@ public class SpanningTreeFinder {
             int u = edge.getVertex1();
             int v = edge.getVertex2();
 
-            if (spanningTree.get(u) && spanningTree.get(v)) continue;
+            if (spanningTree[u] && spanningTree[v]) continue;
 
-            Map<Integer, Integer> tempDegrees = new HashMap<>(degrees);
-            tempDegrees.put(u, tempDegrees.get(u) + 1);
-            tempDegrees.put(v, tempDegrees.get(v) + 1);
+            int[] tempDegrees = Arrays.copyOf(degrees, degrees.length);
+            tempDegrees[u]++;
+            tempDegrees[v]++;
 
-            int leafCount = (int) tempDegrees.values().stream().filter(d -> d == 1).count();
+            int leafCount = 0;
+            for (int deg : tempDegrees) {
+                if (deg == 1) ++leafCount;
+            }
 
             if (leafCount <= maxLeaves) {
                 result.getEdges().add(edge);
                 degrees = tempDegrees;
 
-                int newNode = spanningTree.get(u) ? v : u;
-                spanningTree.put(newNode, true);
+                int newNode = spanningTree[u] ? v : u;
+                spanningTree[newNode] = true;
 
                 for (int i = 1; i <= n; ++i) {
-                    if (!spanningTree.get(i)) {
-                        queue.offer(new Edge(newNode, i, weights.get(newNode).get(i)));
+                    if (!spanningTree[i]) {
+                        queue.offer(new Edge(newNode, i, weights[newNode][i]));
                     }
                 }
             }
         }
 
+        int finalLeafCount = 0;
+        for (int d : degrees) if (d == 1) ++finalLeafCount;
         result.setWeight(result.getEdges().stream().mapToInt(Edge::getWeight).sum());
-        result.setLeaves((int) degrees.values().stream().filter(it -> it == 1).count());
+        result.setLeaves(finalLeafCount);
         localEdgeReplacementOptimization(result, n, maxLeaves, weights);
-        System.out.println(k + " - " + result.getWeight());
+        System.out.println("=================== " + k + " - " + result.getWeight());
 
         return result;
     }
 
-    private void localEdgeReplacementOptimization(Result result, int n, int maxLeaves, Map<Integer, Map<Integer, Integer>> weights) {
+    private void localEdgeReplacementOptimization(Result result, int n, int maxLeaves, int[][] weights) {
         boolean improved = true;
         int count = 0;
+
         while (improved) {
             improved = false;
             ++count;
             System.out.println(count + " : " + result.getWeight());
-            List<Edge> edges = new ArrayList<>(result.getEdges());
-            for (Edge toRemove : edges) {
-                Set<Integer> componentA = bfsWithoutEdge(edges, toRemove.getVertex1(), toRemove);
-                Set<Integer> componentB = new HashSet<>();
-                for (int i = 1; i <= n; ++i) {
-                    if (!componentA.contains(i)) {
-                        componentB.add(i);
+            List<Edge> edges = result.getEdges();
+
+            int[] degrees = new int[n + 1];
+            for (Edge e : edges) {
+                degrees[e.getVertex1()]++;
+                degrees[e.getVertex2()]++;
+            }
+
+            outer:
+            for (int i = 0; i < edges.size(); ++i) {
+                Edge toRemove = edges.get(i);
+
+                DSU dsu = new DSU(n);
+                for (int j = 0; j < edges.size(); ++j) {
+                    if (i != j) {
+                        Edge e = edges.get(j);
+                        dsu.union(e.getVertex1(), e.getVertex2());
                     }
                 }
 
-                for (int u : componentA) {
-                    for (int v : componentB) {
-                        if (u == v) continue;
-                        int weight = weights.get(u).get(v);
+                for (int u = 1; u <= n; ++u) {
+                    for (int v = u + 1; v <= n; ++v) {
+                        if (dsu.connected(u, v)) continue;
+
+                        int weight = weights[u][v];
                         if (weight >= toRemove.getWeight()) continue;
 
-                        List<Edge> newEdges = new ArrayList<>(edges);
-                        newEdges.remove(toRemove);
-                        newEdges.add(new Edge(u, v, weight));
-                        int newLeafCount = computeLeafCount(newEdges, n);
+                        Edge replacement = new Edge(u, v, weight);
+
+                        degrees[toRemove.getVertex1()]--;
+                        degrees[toRemove.getVertex2()]--;
+                        degrees[u]++;
+                        degrees[v]++;
+
+                        int newLeafCount = 0;
+                        for (int d : degrees) {
+                            if (d == 1) ++newLeafCount;
+                        }
+
                         if (newLeafCount <= maxLeaves) {
-                            result.setEdges(newEdges);
-                            result.setWeight(newEdges.stream().mapToInt(Edge::getWeight).sum());
+                            edges.set(i, replacement);
+                            result.setWeight(edges.stream().mapToInt(Edge::getWeight).sum());
                             result.setLeaves(newLeafCount);
                             improved = true;
-                            break;
+                            break outer;
+                        } else {
+                            degrees[toRemove.getVertex1()]++;
+                            degrees[toRemove.getVertex2()]++;
+                            degrees[u]--;
+                            degrees[v]--;
                         }
                     }
-                    if (improved) break;
                 }
             }
         }
     }
 
-    private Set<Integer> bfsWithoutEdge(List<Edge> edges, int start, Edge skipEdge) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-        for (Edge e : edges) {
-            if (e.equals(skipEdge)) continue;
-            adj.computeIfAbsent(e.getVertex1(), k -> new ArrayList<>()).add(e.getVertex2());
-            adj.computeIfAbsent(e.getVertex2(), k -> new ArrayList<>()).add(e.getVertex1());
-        }
+    private static class DSU {
+        int[] parent;
 
-        Set<Integer> visited = new HashSet<>();
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        visited.add(start);
-
-        while (!queue.isEmpty()) {
-            int u = queue.poll();
-            for (int v : adj.getOrDefault(u, Collections.emptyList())) {
-                if (!visited.contains(v)) {
-                    visited.add(v);
-                    queue.add(v);
-                }
+        public DSU(int n) {
+            parent = new int[n + 1];
+            for (int i = 1; i <= n; ++i) {
+                parent[i] = i;
             }
         }
-        return visited;
-    }
 
-    private int computeLeafCount(List<Edge> edges, int n) {
-        Map<Integer, Integer> degrees = new HashMap<>();
-        for (int i = 1; i <= n; ++i) degrees.put(i, 0);
-        for (Edge e : edges) {
-            degrees.put(e.getVertex1(), degrees.get(e.getVertex1()) + 1);
-            degrees.put(e.getVertex2(), degrees.get(e.getVertex2()) + 1);
+        public int find(int x) {
+            if (parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
         }
-        return (int) degrees.values().stream().filter(d -> d == 1).count();
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY)
+                parent[rootY] = rootX;
+        }
+
+        public boolean connected(int x, int y) {
+            return find(x) == find(y);
+        }
     }
 }
